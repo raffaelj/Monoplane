cb981faabbc18da53905085d82ab3be5a67985e9
Raffael <github@raffaeljesche.de>
2019-04-09 23:45:31 +0200

    mini markup fix

--- modules/Monoplane/views/errors/404.php
+++ modules/Monoplane/views/errors/404.php
@@ -1,7 +1,6 @@
-<main>
+
     <h1>@lang('Page not found')</h1>
 
     <p>@lang("Something went wrong. This site doesn't exist.")</p>
 
     <p><a href="@route('/')">@lang('Back to start')</a></p>
-</main>
