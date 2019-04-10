0f6053d667de238b08177308abbd586b5853aaea
Raffael <github@raffaeljesche.de>
2019-04-10 23:37:52 +0200

    suppress errors of dom loader (html5 tags fix/workaround)

--- modules/Monoplane/Helper/Fields.php
+++ modules/Monoplane/Helper/Fields.php
@@ -76,7 +76,12 @@ class Fields extends \Lime\Helper {
         // inspired by https://stackoverflow.com/a/45680712 and
         // https://stackoverflow.com/questions/4879946/how-to-savehtml-of-domdocument-without-html-wrapper#comment86181089_45680712
 
+        // disable errors - workaround for HTML5 tags like "nav" or "header", https://stackoverflow.com/a/6090728
+        libxml_use_internal_errors(true);
+
         $dom->loadHTML('<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head><body>' . $html . '</body></html>');
+
+        libxml_clear_errors();
 
         $anchors = $dom->getElementsByTagName('a');
 
